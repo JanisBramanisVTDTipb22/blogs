3ad32bf22086a820ce7efea14385ba7f44bc92b9
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-27 13:27:23 +0200

    cancer

---
+++ Query #2.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE blog_jānis_bramanis;
+-- USE blog_jānis_bramanis;
+/*
+CREATE TABLE posts (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+title VARCHAR(255) NOT NULL
+);*/
+INSERT INTO posts
+(title)
+VALUES
+("My First Blog Post"),
+("My Second Blog Post");
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?
+<?php
+
+require "functions.php";
+
 echo "hello";
 
-?>
+$connection_string = "mysql:host=localhost;dbname=blog_jānis_bramanis;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($connection_string);
+
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+dd($posts);
