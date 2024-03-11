5940a1a8ac85f9aa2e9452ca170c3f1f629a8b6d
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-11 09:47:11 +0200

    bruh

--- Query #2.sql
+++ Query #2.sql
@@ -5,8 +5,56 @@ CREATE TABLE posts (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 title VARCHAR(255) NOT NULL
 );*/
+USE blog_janis_bramanis;
+
+DROP TABLE posts;
+DROP TABLE categories;
+
+CREATE TABLE posts (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL
+);
+
 INSERT INTO posts
 (title)
 VALUES
 ("My First Blog Post"),
 ("My Second Blog Post");
+
+
+-- Kategoriju funkcionalitāte
+
+-- 1. Izveido tabulu
+CREATE TABLE categories (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	description TEXT,
+);
+
+-- 2. Ievieto datus tabulā, lai vēlāk nav fail
+INSERT INTO categories
+(name)
+VALUES
+("sport"),
+("music"),
+("food");
+
+-- 3. Pievieno category_id posts tabulai
+ALTER TABLE posts 
+ADD category_id INT NOT NULL DEFAULT 1;
+
+-- 4. Izveido ārējo atslēgu
+ALTER TABLE posts
+ADD FOREIGN KEY (category_id) REFERENCES categories(id);
+
+-- 5. Izmaina nepieciešamos category_id
+UPDATE posts
+SET category_id = 3
+WHERE id = 2;
+
+
+SELECT * FROM posts;
+
+SELECT * FROM posts
+JOIN categories
+ON posts.category_id = categories.id;
--- index.php
+++ index.php
@@ -1,37 +1,61 @@
 <?php
-
+// Šis fails ir, lai izvadītu datus no datubāzes uz
+// lapu 
 require "functions.php";
 require "Database.php";
 
-
 $config = require("config.php");
 
-$id = $_GET["id"];
-//dd($id);
-
 $query = "SELECT * FROM posts";
-if (isset($_GET["id"]) && $_GET["id"] !="") {
-    $id = $_GET["id"];
-    $query = "SELECT * FROM posts WHERE id=$id";
-} else {
+$params = [];
 
+if (isset($_GET["id"]) && $_GET["id"] != "") {
+  $id = $_GET["id"];
+  $query .= " WHERE id=:id";
+  $params[":id"] = $id;
+}
+
+if (isset($_GET["category"]) && $_GET["category"] != "") {
+  $category = trim($_GET["category"]);
+  $query .= " JOIN categories
+              ON posts.category_id = categories.id
+              WHERE categories.name = :category
+            ";
+  $params[":category"] = $category;
 }
 
 $db = new Database($config);
 $posts = $db
-            ->execute("SELECT * FROM posts WHERE id=$id")
-            ->fetchAll();
+          ->execute($query, $params)
+          ->fetchAll();
+
 
 echo "<form>";
-echo "<input name='id' />";
-echo "<button>Submit</button>";
-echo "</form>"; 
+echo "<input name='id' value='" . ($_GET["id"] ?? "") ."'/>";
+echo "<button>Filter by ID</button>";
+echo "</form>";
 
- 
-echo "<h1>posts</h1>";
-echo "<h4>I love covid</h4>";
+echo "<form>";
+//$mainigais = ($x < 5) ? "ja" : "ne";
+echo "<input name='category' value='" . ($_GET["category"] ?? "") . "'/>";
+echo "<button>Filter by category</button>";
+echo "</form>";
+
+echo "<form>";
+echo "<Category: ";
+echo "<select name='category'>";
+echo "<option value='sport'>Sport</option>";
+echo "<option value='music'>Music</option>";
+echo "<option value='food'>Food</option>";
+echo "</select>";
+echo "<br>";
+echo "<button>Submit</button>";
+echo "</form>";
+
+echo "<h1>Posts</h1>";
+
 echo "<ul>";
-foreach ($posts as $post) {
-    echo "<li>".$post["title"]."</li>";
+foreach($posts as $post) {
+  echo "<li>" . $post["title"] . "</li>";
 }
 echo "</ul>";
